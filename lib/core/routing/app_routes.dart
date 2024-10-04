7f011604266f5076e0e900248c4511cd1640e523
ibrahimagdy <iibbrraahhimm00@gmail.com>
2024-10-04 08:53:09 +0300

    Finish login cubit & setup DI

--- lib/core/routing/app_routes.dart
+++ lib/core/routing/app_routes.dart
@@ -1,7 +1,10 @@
+import 'package:doctors_app/core/di/dependency_injection.dart';
 import 'package:doctors_app/core/routing/routes.dart';
+import 'package:doctors_app/features/login/logic/login_cubit.dart';
 import 'package:doctors_app/features/login/ui/login_screen.dart';
 import 'package:doctors_app/features/onboarding/onboarding_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRoutes {
   Route generateRoute(RouteSettings settings) {
@@ -12,15 +15,20 @@ class AppRoutes {
         );
       case Routes.loginScreen:
         return MaterialPageRoute(
-          builder: (context) => const LoginScreen(),
+          builder: (context) =>
+              BlocProvider(
+                create: (context) => getIt<LoginCubit>(),
+                child: const LoginScreen(),
+              ),
         );
       default:
         return MaterialPageRoute(
-          builder: (context) => Scaffold(
-            body: Center(
-              child: Text('No route defined for ${settings.name}'),
-            ),
-          ),
+          builder: (context) =>
+              Scaffold(
+                body: Center(
+                  child: Text('No route defined for ${settings.name}'),
+                ),
+              ),
         );
     }
   }
