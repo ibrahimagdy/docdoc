30b313313cfd56ba75f3d173e7a9fb111a4fee80
ibrahimagdy <iibbrraahhimm00@gmail.com>
2024-10-09 17:10:56 +0300

    Add Development & Production flavors for Android

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -2,9 +2,11 @@ import 'package:doctors_app/core/di/dependency_injection.dart';
 import 'package:doctors_app/core/routing/app_routes.dart';
 import 'package:doctors_app/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async{
   setupGetIt();
+  await ScreenUtil.ensureScreenSize();
   runApp(
     DocApp(
       appRoutes: AppRoutes(),
